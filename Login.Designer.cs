a282623d8dd0867b11232ba142617a0dbb9bbf2a
Mihika <mihikamadhavi123@gmail.com>
2026-07-03 20:25:51 +0530

    Added Login form

---
+++ Login.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PostalDepHrSystem
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nic = new System.Windows.Forms.TextBox();
+            this.txt_pw = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_signIn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PostalDepHrSystem.Resource1.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(753, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PostalDepHrSystem.Properties.Resources.slpost1;
+            this.pictureBox2.Location = new System.Drawing.Point(29, 124);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(191, 301);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl.Location = new System.Drawing.Point(282, 166);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(96, 38);
+            this.lbl.TabIndex = 8;
+            this.lbl.Text = "Login";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(289, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "NIC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Firebrick;
+            this.label2.Location = new System.Drawing.Point(289, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Password";
+            // 
+            // txt_nic
+            // 
+            this.txt_nic.Location = new System.Drawing.Point(372, 223);
+            this.txt_nic.Name = "txt_nic";
+            this.txt_nic.Size = new System.Drawing.Size(376, 22);
+            this.txt_nic.TabIndex = 11;
+            // 
+            // txt_pw
+            // 
+            this.txt_pw.Location = new System.Drawing.Point(372, 262);
+            this.txt_pw.Name = "txt_pw";
+            this.txt_pw.Size = new System.Drawing.Size(376, 22);
+            this.txt_pw.TabIndex = 12;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn_login.Location = new System.Drawing.Point(292, 312);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(456, 35);
+            this.btn_login.TabIndex = 13;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = false;
+            // 
+            // btn_signIn
+            // 
+            this.btn_signIn.Location = new System.Drawing.Point(292, 353);
+            this.btn_signIn.Name = "btn_signIn";
+            this.btn_signIn.Size = new System.Drawing.Size(459, 27);
+            this.btn_signIn.TabIndex = 14;
+            this.btn_signIn.Text = "Don\'t you have an Account? SIgn In";
+            this.btn_signIn.UseVisualStyleBackColor = true;
+            this.btn_signIn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(833, 460);
+            this.Controls.Add(this.btn_signIn);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_pw);
+            this.Controls.Add(this.txt_nic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nic;
+        private System.Windows.Forms.TextBox txt_pw;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_signIn;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PostalDepHrSystem
+{
+    public partial class Login : Form
+    {
+        // Database connection string
+        private string connStr = "server=localhost;port=3306;username=root;password=;database=Postal_HR_System;";
+
+        public Login()
+        {
+            InitializeComponent();
+
+            // Set password character to hide password
+            if (txt_pw != null)
+            {
+                txt_pw.PasswordChar = '*';
+            }
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            this.btn_signIn.Click += new System.EventHandler(this.button1_Click);
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string nic = txt_nic.Text.Trim();
+            string password = txt_pw.Text.Trim();
+
+            // Check if fields are empty
+            if (string.IsNullOrEmpty(nic) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both NIC and Password.", "Validation Required",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txt_nic.Focus();
+                return;
+            }
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    // Get user details including Division
+                    string query = "SELECT * FROM Users WHERE NIC = @nic AND Password = @password";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@nic", nic);
+                        cmd.Parameters.AddWithValue("@password", password);
+
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Get user info
+                                string firstName = reader["FirstName"].ToString();
+                                string division = reader["Division"].ToString();
+                                string designation = reader["Designation"].ToString();
+
+                                // Login successful
+                                MessageBox.Show($"Welcome {firstName}!", "Login Successful",
+                                              MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                // Redirect based on Division
+                                RedirectUserByDivision(division, firstName, designation, nic);
+                                this.Hide();
+                            }
+                            else
+                            {
+                                // Login failed
+                                MessageBox.Show("Invalid NIC or Password!\n\nMake sure you entered:\nNIC: " + nic + "\nPassword: " + password,
+                                              "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                                // Clear fields
+                                txt_nic.Clear();
+                                txt_pw.Clear();
+                                txt_nic.Focus();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"Database Error: {ex.Message}\n\nMake sure XAMPP MySQL is running.",
+                              "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RedirectUserByDivision(string division, string firstName, string designation, string nic)
+        {
+            // Debug - Show what division we got
+            MessageBox.Show($"Debug: Division from database is: '{division}'", "Debug");
+
+            // Check for Administration (Admin)
+            if (division == "Administration")
+            {
+                MessageBox.Show("Opening Admin Dashboard...", "Debug");
+                adminDashboard adminForm = new adminDashboard();
+                adminForm.Show();
+            }
+            else if (division == "IT Department" || division == "IT")
+            {
+                MessageBox.Show($"Welcome {firstName} from IT Department!\n\nOpening IT Dashboard...", "Information");
+                // Open IT Dashboard or Admin Dashboard temporarily
+                adminDashboard adminForm = new adminDashboard();
+                adminForm.Show();
+            }
+            else if (division == "Human Resources" || division == "HR")
+            {
+                MessageBox.Show($"Welcome {firstName} from HR Department!\n\nOpening HR Dashboard...", "Information");
+                HRDashboard hrForm = new HRDashboard();  
+                hrForm.Show();
+            }
+            else if (division == "Finance")
+            {
+                MessageBox.Show($"Welcome {firstName} from Finance Department!\n\nOpening Finance Dashboard...", "Information");
+                adminDashboard adminForm = new adminDashboard();
+                adminForm.Show();
+            }
+            else if (division == "Operations")
+            {
+                MessageBox.Show($"Welcome {firstName} from Operations Department!\n\nOpening Operations Dashboard...", "Information");
+                adminDashboard adminForm = new adminDashboard();
+                adminForm.Show();
+            }
+            else if (division == "Sales")
+            {
+                MessageBox.Show($"Welcome {firstName} from Sales Department!\n\nOpening Sales Dashboard...", "Information");
+                adminDashboard adminForm = new adminDashboard();
+                adminForm.Show();
+            }
+            else
+            {
+                MessageBox.Show($"Welcome {firstName} from {division}!\n\nOpening General Dashboard...", "Information");
+                adminDashboard adminForm = new adminDashboard();
+                adminForm.Show();
+            }
+        }
+
+        // Press Enter key to login
+        private void txt_pw_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btn_login_Click(sender, e);
+            }
+        }
+
+        // Form Load event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_nic.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sign_Up signUpForm = new Sign_Up(); 
+            signUpForm.Show();
+            this.Hide();
+        }
+    }
+}
